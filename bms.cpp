a9649f99ee03ac17c4a553ba1484c58b06374726
Andy <s1416765@sms.ed.ac.uk>
2017-11-18 15:10:18 +0000

    Change bms

--- bms.cpp
+++ bms.cpp
@@ -1,4 +1,10 @@
 #include "bms.h"
 
         BatteryManagementSystem::BatteryManagementSystem()
+	
+            double BatteryManagementSystem::get_current()
+            {
+              return 0;
+            }
+            
 
